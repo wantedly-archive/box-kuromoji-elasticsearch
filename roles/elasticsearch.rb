2e846f599796b16c382fd936af1003eb85d3ab9f
Seigo Uchida <spesnova@gmail.com>
2014-02-10 16:39:02 +0900

    Move to elasticsearch run list to role.

---
+++ roles/elasticsearch.rb
@@ -0,0 +1,24 @@
+name "elasticsearch"
+run_list(
+  "recipe[apt]",
+  "recipe[build-essential]",
+  "recipe[java]",
+  "recipe[elasticsearch]",
+  "recipe[elasticsearch::plugins]"
+)
+override_attributes({
+  "apt" => {
+    "mirror" => "us-east-1.ec2"
+  },
+  "elasticsearch" => {
+    "version" => "0.90.10",
+    "plugins" => {
+      "elasticsearch/elasticsearch-analysis-kuromoji/1.7.0" => {}
+    }
+  },
+  "java" => {
+    "install_flavor" => "openjdk",
+    "jdk_version" => "7"
+  }
+})
+
